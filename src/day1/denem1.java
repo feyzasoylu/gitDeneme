14a5461f5f8d4ff0ea9a317068ac0eba76b94743
feyzasoylu <fyznr.soylu@gmail.com>
2020-11-10 12:29:56 -0500

    last changes are made.

---
+++ src/day1/denem1.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class denem1 {
+    public static void main(String[] args) {
+        System.out.println("deneme");
+        System.out.println("i need to fix this message.");
+        int a =5;
+        int b=8;
+        int c= b+a;
+        System.out.println(c);
+    }
+}
---
+++ src/day2/deneme2.java
@@ -0,0 +1,7 @@
+package day2;
+
+public class deneme2 {
+    public static void main(String[] args) {
+        System.out.println("this is easy");
+    }
+}
